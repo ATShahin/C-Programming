26b7101d80001be121997de8fb32b06cf90cef16
ATShahin <162863534+ATShahin@users.noreply.github.com>
2024-03-10 13:34:10 +0530

    Update snprintf.c

--- 100 days C learning/Day2/snprintf.c
+++ 100 days C learning/Day2/snprintf.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+#include"file.h"
+
+/* Size of the buffer for received data*/
+#define BUFLEN  800
+
+/*********************************************************************
+*
+*       main()
+*
+*  Function description
+*   Application entry point.
+*/
+
+
+
+const static char pName[] = "EWG";
+const static char copyRight[] = "(c)";
+const static char companyNameplt[] = "AUMA";
+const static unsigned int u16Year=2023;
+const static char version[]= "V00.00";
+
+char u8MsgBuff[BUFLEN];
+
+
+int main(void) {
+  
+
+  snprintf(u8MsgBuff, sizeof(u8MsgBuff), "\r\n%s %s %s %d \r\n%s \r\n",pName,copyRight,companyNameplt,u16Year,version);
+
+  printf(u8MsgBuff);
+
+}
+
+/*************************** End of file ****************************/
