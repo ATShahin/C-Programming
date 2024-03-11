9c323fa6d4682fc2e54dd7f8c7e7de7fb58ccde6
ATShahin <162863534+ATShahin@users.noreply.github.com>
2024-03-11 22:06:08 +0530

    Update strings.c

--- 100 days C learning/Day3/strings.c
+++ 100 days C learning/Day3/strings.c
@@ -2,14 +2,24 @@
 #include<string.h>
 
 char string1[]= "Hello";
+char string2[]= "Programmer";
+
+char string3[20];
 
 int main()
 {
  int size;
 
  size = strlen(string1);
+ printf("length is %d\n",size);
 
- printf("length is %d",size);
+ strcpy(string3, string1);
+ printf("String3 contents is : %s\n",string3);
+
+ if (strncmp(string1,string3,5) ==0)
+  printf("Strings 1 and 3 match\n");
+ else
+  printf("Strings 1 and 3 no match\n");
 
  return 0;
 }
