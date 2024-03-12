77b540b4213583744499aacdd77ff751436036a7
ATShahin <162863534+ATShahin@users.noreply.github.com>
2024-03-12 10:37:06 +0530

    Update StringCharArray.c

--- 100 days C learning/Day4/StringCharArray.c
+++ 100 days C learning/Day4/StringCharArray.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+#include<string.h>
 
+char string1[]= "Hello";
+char array[5] ={'H','E','L','L','O'};
+
+int main()
+{
+  printf("string is %s\n",&string1[0]);
+ 
+  printf("Array:\n");
+  for (int i=0;i<=4;i++)
+  {
+   printf("%x\n",&array[i]);
+  // printf("\n");
+  }
+  printf("\n");
+
+ return 0;
+}
