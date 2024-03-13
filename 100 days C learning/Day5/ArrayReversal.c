409a00a466d85b955337a7215fc49009aeed400f
ATShahin <162863534+ATShahin@users.noreply.github.com>
2024-03-13 08:11:32 +0530

    Update ArrayReversal.c

---
+++ 100 days C learning/Day5/ArrayReversal.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+
+char string[]= "Hello C";
+char array[];
+
+int count= 0;
+int k=0;
+int main()
+{
+ for (int i=0;string[]!=0;i++)
+ {
+  string++;
+  count++;
+ }
+ for(int j=(count-1);j<=0;j--)
+ {
+  array[k]= string[j];
+  printf("array: %c",array[k];
+ }
+
+}
